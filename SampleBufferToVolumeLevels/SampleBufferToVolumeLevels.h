326cca95bba3dc515c63d9a0479ff5df88e44296
阳眼的熊 <237505990@qq.com>
2017-12-08 12:59:13 +0800

    Initial commit

---
+++ SampleBufferToVolumeLevels/SampleBufferToVolumeLevels.h
@@ -0,0 +1,19 @@
+//
+//  SampleBufferToVolumeLevels.h
+//  SampleBufferToVolumeLevels
+//
+//  Created by huyangyang on 2017/12/8.
+//  Copyright © 2017年 HuYangYang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleBufferToVolumeLevels.
+FOUNDATION_EXPORT double SampleBufferToVolumeLevelsVersionNumber;
+
+//! Project version string for SampleBufferToVolumeLevels.
+FOUNDATION_EXPORT const unsigned char SampleBufferToVolumeLevelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleBufferToVolumeLevels/PublicHeader.h>
+
+
---
+++ SampleBufferToVolumeLevels/SampleBufferToVolumeLevelsEngine.swift
@@ -0,0 +1,54 @@
+//
+//  SampleBufferToVolumeLevelsEngine.swift
+//  SampleBufferToVolumeLevels
+//
+//  Created by huyangyang on 2017/12/8.
+//  Copyright © 2017年 HuYangYang.com. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+class SampleBufferToVolumeLevelsEngine: NSObject {
+
+    class func getVolumeLevelsFromeSampleBuffer(sampleBuffer : CMSampleBuffer) -> Float {
+        var buffer: CMBlockBuffer? = nil
+        
+        // Needs to be initialized somehow, even if we take only the address
+        var audioBufferList = AudioBufferList(mNumberBuffers: 1,
+                                              mBuffers: AudioBuffer(mNumberChannels: 1, mDataByteSize: 0, mData: nil))
+        
+        CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
+            sampleBuffer,
+            nil,
+            &audioBufferList,
+            MemoryLayout<AudioBufferList>.size,
+            nil,
+            nil,
+            UInt32(kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment),
+            &buffer
+        )
+        
+        let abl = UnsafeMutableAudioBufferListPointer(&audioBufferList)
+
+
+
+        for buffer in abl {
+
+            
+            let samplesCount = Int(buffer.mDataByteSize) / MemoryLayout<Int16>.size
+            let samplesPointer = audioBufferList.mBuffers.mData!.bindMemory(to: Int16.self, capacity: samplesCount)
+            let samples = UnsafeMutableBufferPointer<Int16>(start: samplesPointer, count: samplesCount)
+
+            var sum:Int64 = 0
+            
+            for sample in samples {
+                let s = Int64(sample)
+                sum = (sum + s*s)
+            }
+            return (sqrt(Float(sum/Int64(samples.count))))
+        }
+        return 0.0;
+        
+    }
+    
+}
